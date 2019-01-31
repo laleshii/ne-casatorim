5709a8eae2edf44c0401d0372a32bfdbde94397f
Rares Sfirlogea <rrr.junior@gmail.com>
2019-01-31 16:31:13 +0100

    Initial implementation

---
+++ components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <img class="main-image lg:w-1/2 w-full" src="~/assets/images/main.jpg" />
+</template>
+
+<style scoped>
+.main-image {
+  display: inline-block;
+  position: relative;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <div class="mt-8 mb-8">
+      <h1 class="font-pennellino text-6xl">
+        Talida & Rares
+      </h1>
+      <logo />
+      <h1 class="font-pennellino text-5xl">
+        24 August 2019
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+</style>
